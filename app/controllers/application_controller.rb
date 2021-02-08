fe31c16f44b9358a643a4d0d7787f3cb08dea331
Vell - [I am available to join projects.] <vmcilwain@me.com>
2021-02-08 08:42:53 -0500

    add devise with user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :error
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
 end
